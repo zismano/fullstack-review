10ace33e39084c9620743bc629a6a15d488495d0
Ofir Zisman <zismanofir@gmail.com>
2018-01-02 13:59:42 -0800

    Add schema and implement save function

--- database/index.js
+++ database/index.js
@@ -3,14 +3,31 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  userId: Number,
+  userName: String,
+  repoId: Number,
+  repoName: String,
+  updatedAt: Date
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (userId, userName, repoId, repoName, updatedAt) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let newRepo = new Repo({
+  	userId,
+  	userName,
+  	repoId,
+  	repoName,
+  	updatedAt
+  });
+  newRepo.save(err => {
+  	if (err) {
+  		console.log(`Error ${err}`);
+  	}
+  });
 }
 
 module.exports.save = save;
